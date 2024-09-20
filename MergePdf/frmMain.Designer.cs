ea1bd3b2780453f2a79dc29bacccaf30f0d8cb12
Sadegh Dehghani <dehghani.prg@outlook.com>
2024-09-20 10:15:24 +0330

    Init

---
+++ MergePdf/frmMain.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace MergePdf
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.folderBrowserDialogMain = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnFolderBrowse = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMerge.Location = new System.Drawing.Point(482, 174);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(202, 34);
+            this.btnMerge.TabIndex = 0;
+            this.btnMerge.Text = "ادغام PDF فایل ها";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // btnFolderBrowse
+            // 
+            this.btnFolderBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFolderBrowse.Location = new System.Drawing.Point(482, 12);
+            this.btnFolderBrowse.Name = "btnFolderBrowse";
+            this.btnFolderBrowse.Size = new System.Drawing.Size(202, 33);
+            this.btnFolderBrowse.TabIndex = 1;
+            this.btnFolderBrowse.Text = "انتخاب پوشه مورد نظر";
+            this.btnFolderBrowse.UseVisualStyleBackColor = true;
+            this.btnFolderBrowse.Click += new System.EventHandler(this.btnFolderBrowse_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(15, 121);
+            this.txtPath.Multiline = true;
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(669, 38);
+            this.txtPath.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 233);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(696, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(227, 17);
+            this.toolStripStatusLabel1.Text = "تهیه و تنظیم توسط: مهندس صادق دهقانی ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(559, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "مسیر  پوشه انتخاب شده ";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 255);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnFolderBrowse);
+            this.Controls.Add(this.btnMerge);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ادغام فایل های PDF";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogMain;
+        private System.Windows.Forms.Button btnFolderBrowse;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ MergePdf/frmMain.cs
@@ -0,0 +1,127 @@
+﻿using Spire.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace MergePdf
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public static string PathSelected;
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+
+            //// Create a document for the merged result.
+            //Document mergedDocument = new Document();
+
+            //// Keep a list of input streams to close when done.
+            //List<FileStream> streams = new List<FileStream>();
+            //foreach (FileInfo fileInfo in allPDFs)
+            //{
+            //    // Open input stream and add to list of streams.
+            //    FileStream stream = new FileStream(fileInfo.FullName, FileMode.Open, FileAccess.Read);
+            //    streams.Add(stream);
+
+            //    // Open input document.
+            //    Document document = new Document(stream);
+
+            //    // Append all pages to target document.
+            //    // The target document holds references to data in the input stream.
+            //    // For efficiency it does not copy this data so the input streams
+            //    // should not be closed before the merged document is saved.
+            //    mergedDocument.Pages.AddRange(document.Pages.CloneToArray());
+            //   }
+
+            //// Save merged document.
+            //using (FileStream output = new FileStream(@"..\..\output.pdf", FileMode.Create, FileAccess.Write))
+            //{
+            //    mergedDocument.Write(output);
+            //}
+
+            //// Close all input streams.
+            //streams.ForEach(stream => stream.Close());
+
+
+
+
+            try
+            {
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(PathSelected);
+
+                FileInfo[] allPDFs = directoryInfo.GetFiles("*.pdf");
+
+                List<string> files = new List<string>();
+
+
+                foreach (var item in allPDFs)
+                {
+                    files.Add(item.FullName);
+                }
+
+                var fileArray = files.ToArray();
+                PdfDocumentBase doc = PdfDocument.MergeFiles(fileArray);
+
+
+                string Outpath=PathSelected+"\\output.pdf";
+
+                doc.Save(fileName: Outpath, FileFormat.PDF);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            btnMerge.Enabled = false;
+
+        }
+
+        private void btnFolderBrowse_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialogMain.ShowDialog();
+
+            PathSelected = folderBrowserDialogMain.SelectedPath;
+
+            if (PathSelected != null)
+            {
+                txtPath.Text = PathSelected;
+                btnMerge.Enabled = true;
+            }
+
+
+        }
+    }
+}
